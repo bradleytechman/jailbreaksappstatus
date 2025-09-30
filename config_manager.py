767c6e2d76073f999e0b86f8adab7be58bee4f5b
bradleytechman <74463271+bradleytechman@users.noreply.github.com>
2025-09-30 19:54:29 -0400

    update with local code
    Refactor GuildConfigManager and add ConfigManager methods for loading and saving configuration.

--- config_manager.py
+++ config_manager.py
@@ -1,51 +1,15 @@
 import json
-import os
 
-CONFIG_FILE = "guild_config.json"
-
-class GuildConfigManager:
-    def __init__(self):
-        if not os.path.exists(CONFIG_FILE):
-            with open(CONFIG_FILE, "w") as f:
-                json.dump({}, f)
-        self._load()
-
-    def _load(self):
-        with open(CONFIG_FILE, "r") as f:
-            try:
-                self.data = json.load(f)
-            except json.JSONDecodeError:
-                self.data = {}
-
-    def _save(self):
-        with open(CONFIG_FILE, "w") as f:
-            json.dump(self.data, f, indent=4)
-
-    def get_guild_config(self, guild_id: int):
-        return self.data.get(str(guild_id), {})
-
-    def set_guild_config(self, guild_id: int, channel_id=None, ping_role_id=None, access_role_id=None):
-        gid = str(guild_id)
-        if gid not in self.data:
-            self.data[gid] = {}
-        if channel_id is not None:
-            self.data[gid]["channel_id"] = channel_id
-        if ping_role_id is not None:
-            self.data[gid]["ping_role_id"] = ping_role_id
-        if access_role_id is not None:
-            self.data[gid]["access_role_id"] = access_role_id
-        self._save()
-
-
-# add ConfigManager so status.py still works
 class ConfigManager:
     @staticmethod
     def load_config():
-        if not os.path.exists(CONFIG_FILE):
-            with open(CONFIG_FILE, "w") as f:
-                json.dump({}, f)
-        with open(CONFIG_FILE, "r") as f:
-            try:
+        try:
+            with open("config.json", "r") as f:
                 return json.load(f)
-            except json.JSONDecodeError:
-                return {}
+        except FileNotFoundError:
+            return {}
+
+    @staticmethod
+    def save_config(config):
+        with open("config.json", "w") as f:
+            json.dump(config, f, indent=4)
